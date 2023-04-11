c3714002e855acae5de532f9f41e590d8a548cc0
兔子 <119795593+unti-io@users.noreply.github.com>
2023-04-11 13:39:28 +0800

    v1.0.6
    v1.0.6

--- utils/file.go
+++ utils/file.go
@@ -11,8 +11,10 @@ import (
 type FileRequest struct {
 	// 文件名
 	Name string
-	// 文件目录
+	// 文件路径
 	Path string
+	// 目录路径
+	Dir string
 	// 文件后缀
 	Ext string
 }
@@ -27,6 +29,7 @@ type FileResponse struct {
 	Error  error
 	Result string
 	Byte   []byte
+	Slice  []any
 }
 
 func File(model ...FileStruct) *FileStruct {
@@ -41,6 +44,12 @@ func File(model ...FileStruct) *FileStruct {
 // Path 设置文件路径(包含文件名，如：/tmp/test.txt)
 func (this *FileStruct) Path(path any) *FileStruct {
 	this.request.Path = cast.ToString(path)
+	return this
+}
+
+// Dir 设置目录路径(不包含文件名，如：/tmp)
+func (this *FileStruct) Dir(dir any) *FileStruct {
+	this.request.Dir = cast.ToString(dir)
 	return this
 }
 
@@ -156,7 +165,7 @@ func (this *FileStruct) Byte(path ...any) (result *FileResponse) {
 }
 
 // FileList 获取指定目录下的所有文件
-func FileList(path any, opt ...map[string]any) (slice []string) {
+func (this *FileStruct) List(opt ...map[string]any) (result *FileResponse) {
 
 	// 默认参数
 	defOpt := map[string]any{
@@ -170,6 +179,8 @@ func FileList(path any, opt ...map[string]any) (slice []string) {
 		"domain": "",
 		// 过滤前缀
 		"prefix": "",
+		// 目录路径
+		"dir": "",
 	}
 
 	if len(opt) != 0 {
@@ -185,7 +196,17 @@ func FileList(path any, opt ...map[string]any) (slice []string) {
 	}
 
 	conf := opt[0]
-	err := filepath.Walk(cast.ToString(path), func(path string, info os.FileInfo, err error) error {
+
+	if !IsEmpty(conf["dir"]) {
+		this.request.Dir = cast.ToString(conf["dir"])
+	}
+
+	if IsEmpty(this.request.Dir) {
+		this.response.Error = errors.New("目录路径不能为空")
+		return this.response
+	}
+	var slice []string
+	this.response.Error = filepath.Walk(this.request.Dir, func(path string, info os.FileInfo, err error) error {
 		// 忽略当前目录
 		if info.IsDir() {
 			return nil
@@ -207,21 +228,87 @@ func FileList(path any, opt ...map[string]any) (slice []string) {
 		return nil
 	})
 
-	if err != nil {
-		return []string{}
-	}
-
 	// 转码为网络路径
 	if conf["format"] == "network" {
 		for key, val := range slice {
 			slice[key] = filepath.ToSlash(val)
 			if !IsEmpty(conf["domain"]) {
-				// root, _ := os.Getwd()
-				// slice[key] = cast.ToString(conf["domain"]) + slice[key][len(root) + len(cast.ToString(conf["prefix"])):]
 				slice[key] = cast.ToString(conf["domain"]) + slice[key][len(cast.ToString(conf["prefix"])):]
 			}
 		}
 	}
 
-	return
+	this.response.Slice = cast.ToSlice(slice)
+	return this.response
 }
+
+// // FileList 获取指定目录下的所有文件
+// func FileList(path any, opt ...map[string]any) (slice []string) {
+//
+// 	// 默认参数
+// 	defOpt := map[string]any{
+// 		// 获取指定后缀的文件
+// 		"ext": []string{"*"},
+// 		// 包含子目录
+// 		"sub": true,
+// 		// 返回路径格式
+// 		"format": "network",
+// 		// 域名
+// 		"domain": "",
+// 		// 过滤前缀
+// 		"prefix": "",
+// 	}
+//
+// 	if len(opt) != 0 {
+// 		// 合并参数
+// 		for key, val := range defOpt {
+// 			if opt[0][key] == nil {
+// 				opt[0][key] = val
+// 			}
+// 		}
+// 	} else {
+// 		// 默认参数
+// 		opt = append(opt, defOpt)
+// 	}
+//
+// 	conf := opt[0]
+// 	err := filepath.Walk(cast.ToString(path), func(path string, info os.FileInfo, err error) error {
+// 		// 忽略当前目录
+// 		if info.IsDir() {
+// 			return nil
+// 		}
+// 		// 忽略子目录
+// 		if !conf["sub"].(bool) && filepath.Dir(path) != path {
+// 			return nil
+// 		}
+// 		// []string 转 []any
+// 		var exts []any
+// 		for _, v := range conf["ext"].([]string) {
+// 			exts = append(exts, v)
+// 		}
+// 		// 忽略指定后缀
+// 		if !InArray("*", exts) && !InArray(filepath.Ext(path), exts) {
+// 			return nil
+// 		}
+// 		slice = append(slice, path)
+// 		return nil
+// 	})
+//
+// 	if err != nil {
+// 		return []string{}
+// 	}
+//
+// 	// 转码为网络路径
+// 	if conf["format"] == "network" {
+// 		for key, val := range slice {
+// 			slice[key] = filepath.ToSlash(val)
+// 			if !IsEmpty(conf["domain"]) {
+// 				// root, _ := os.Getwd()
+// 				// slice[key] = cast.ToString(conf["domain"]) + slice[key][len(root) + len(cast.ToString(conf["prefix"])):]
+// 				slice[key] = cast.ToString(conf["domain"]) + slice[key][len(cast.ToString(conf["prefix"])):]
+// 			}
+// 		}
+// 	}
+//
+// 	return
+// }
