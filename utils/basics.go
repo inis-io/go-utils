e5c084f15991f42f18172a1d7bf23e5e2107d4c8
兔子 <119795593+unti-io@users.noreply.github.com>
2023-04-19 20:24:38 +0800

    v1.1.0
    v1.1.0

--- utils/basics.go
+++ utils/basics.go
@@ -1,6 +1,10 @@
 package utils
 
-import "runtime"
+import (
+	"github.com/spf13/cast"
+	"runtime"
+	"strings"
+)
 
 // ForMap - 遍历数组，返回新数组
 func ForMap[T any](slice []T, fun func(item T) (result T)) (newSlice []T) {
@@ -10,13 +14,21 @@ func ForMap[T any](slice []T, fun func(item T) (result T)) (newSlice []T) {
 	return slice
 }
 
-// Ternary
-// 三元运算符
+// Ternary - 三元运算符
 func Ternary[T any](IF bool, TRUE T, FALSE T) T {
 	if IF {
 		return TRUE
 	}
 	return FALSE
+}
+
+// Replace - 字符串替换
+func Replace(value any, params map[string]any) (result string) {
+	result = cast.ToString(value)
+	for key, val := range params {
+		result = strings.Replace(result, key, cast.ToString(val), -1)
+	}
+	return result
 }
 
 func GetCaller() (funcName string, fileName string, line int) {
