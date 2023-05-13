1f83977e03df77961472c2cb050b6efdd4fa9c64
兔子 <unti.io66@gmail.com>
2023-05-13 18:35:49 +0800

    v1.1.2
    v1.1.2

--- utils/in.go
+++ utils/in.go
@@ -1,13 +1,29 @@
 package utils
 
-import "reflect"
+import (
+	"reflect"
+	"sync"
+)
 
 // InArray - 判断某个值是否在数组中
 func InArray[T any](value T, array []T) (ok bool) {
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
+	found := false
 	for _, val := range array {
-		if reflect.DeepEqual(value, val) {
-			return true
+		if found {
+			break
 		}
+		wg.Add(1)
+		go func(item T) {
+			defer wg.Done()
+			if reflect.DeepEqual(value, item) {
+				mutex.Lock()
+				found = true
+				mutex.Unlock()
+			}
+		}(val)
 	}
-	return false
+	wg.Wait()
+	return found
 }
